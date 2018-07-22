6de56bbacaf3304679bcb9ecf1108fc45c4610fd
Brian Peguero <brianpeguero@gmail.com>
2018-07-22 19:08:52 -0400

    Created menu items

--- Text-Editor.py
+++ Text-Editor.py
@@ -1,10 +1,17 @@
 import Tkinter
 import tkFileDialog
 
+app = Tkinter.Tk(className="Text Editor")
+
+#adds a textbox
+text = Tkinter.Text(app)
+
+
 #a dummy method
 def dummy():
     print("I'm a dummy method")
 
+#file menu commands
 def openCommand():
     file = tkFileDialog.askopenfile(parent = app, mode ="rb", title="select a file")
     if file != None:
@@ -15,19 +22,17 @@ def openCommand():
 def newCommand():
     print ("dummy")
 
-def saveCommand(self):
+def saveCommand():
     file = tkFileDialog.asksaveasfile(mode = "w")
     if file != None:
+
         file.write()
         file.close()
 
 def exitCommand():
-    print("Dummy")
+    app.destroy()
 
-app = Tkinter.Tk(className="Text Editor")
 
-#adds a textbox
-text = Tkinter.Text(app)
 
 #creates a menu for the text editor
 menu = Tkinter.Menu(app)
@@ -36,12 +41,12 @@ filemenu = Tkinter.Menu(menu)
 
 menu.add_cascade(label = "File", menu = filemenu)
 filemenu.add_command(label="New", command = dummy())
-filemenu.add_command(label="Open", command = dummy())
-filemenu.add_command(label="Save", command = dummy())
+filemenu.add_command(label="Open", command = openCommand())
+filemenu.add_command(label="Save", command = saveCommand())
 
 #____________________________________________________
 filemenu.add_separator()
-filemenu.add_command(label="Exit", command = dummy())
+filemenu.add_command(label="Exit", command = exitCommand())
 
 text.grid()
 app.mainloop()
