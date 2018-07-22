6b1943fad6c218d0ca5a14bf09d4bab1b207909c
Brian Peguero <brianpeguero@gmail.com>
2018-07-22 18:56:11 -0400

    Created an Editor with a text feild using Tkinter.

--- Text-Editor.py
+++ Text-Editor.py
@@ -1,5 +1,47 @@
-import Tinker 
+import Tkinter
+import tkFileDialog
 
-def main():
+#a dummy method
+def dummy():
+    print("I'm a dummy method")
 
-main()
+def openCommand():
+    file = tkFileDialog.askopenfile(parent = app, mode ="rb", title="select a file")
+    if file != None:
+        contents = file.read()
+        text.insert("1.0", contents)
+        file.close()
+
+def newCommand():
+    print ("dummy")
+
+def saveCommand(self):
+    file = tkFileDialog.asksaveasfile(mode = "w")
+    if file != None:
+        file.write()
+        file.close()
+
+def exitCommand():
+    print("Dummy")
+
+app = Tkinter.Tk(className="Text Editor")
+
+#adds a textbox
+text = Tkinter.Text(app)
+
+#creates a menu for the text editor
+menu = Tkinter.Menu(app)
+app.config(menu=menu)
+filemenu = Tkinter.Menu(menu)
+
+menu.add_cascade(label = "File", menu = filemenu)
+filemenu.add_command(label="New", command = dummy())
+filemenu.add_command(label="Open", command = dummy())
+filemenu.add_command(label="Save", command = dummy())
+
+#____________________________________________________
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command = dummy())
+
+text.grid()
+app.mainloop()
